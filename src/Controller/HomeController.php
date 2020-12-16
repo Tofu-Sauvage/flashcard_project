e9114e2d21ce17a8ce8f9a882ecbc9870bbda04b
Quentin <pupier.quentin@gmail.com>
2020-12-16 16:39:17 +0100

    Formulaire d'inscription + back (en cours)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\InscriptionType;
+use App\Service\FormsManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController {
 
-  public function homeAction() {
-    
-    return $this->render("pages/accueil.html.twig");
+  public function homeAction(Request $request, EntityManagerInterface $em) {
+    $user = new User();
+    $form = $this->createForm(InscriptionType::class);
+
+    $form->handleRequest($request);
+    if($form->isSubmitted()){
+      $user = $form->getData();
+      $image = $form->get('image')->getData();
+
+      if($image){
+        $newFileName ="user-" . uniqid(). "." . $image->guessExtension();
+        $image->move($this->getParameter('uploads'), $newFileName);
+        $user->setImage($newFileName);
+      }
+
+      $user->setCreatedAt(date('now'));
+      $user->setRoles(['ROLE_USER']);
+
+        $em->persist($user);
+        $em->flush();
+        return $this->redirectToRoute('index');
+    }
+    return $this->render("pages/accueil.html.twig", ['form' => $form->createView()]);
   }
 }
