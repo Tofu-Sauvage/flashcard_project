7026ed76f1f948ce13ebd31b8666f520c659c567
Charlotte <charlotte_sch@icloud.com>
2020-12-17 14:30:54 +0100

    Formulaires créa + modif des langues et catégories

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoriesController extends AbstractController {
@@ -10,5 +13,20 @@ class CategoriesController extends AbstractController {
   public function indexAction(CategoryRepository $categoryRepository) {
     $categories = $categoryRepository->findAll();
     return $this->render('./pages/administration/categories.html.twig', ['categories'=>$categories]);
+  }
+
+  public function categoryCreateAction(Request $request, EntityManagerInterface $em)
+  {
+    $form = $this->createForm(CategoryType::class);
+    $form->handleRequest($request);
+    if ($form->isSubmitted()) {
+      $category = $form->getData();
+
+      $em->persist($category);
+      $em->flush();
+      //$this->addFlash('success-category', 'La catégorie a bien été ajoutée !');
+      return $this->redirectToRoute('admin-categories');
+    }
+    return $this->render('./pages/administration/categoryForm.html.twig', ['categoryForm' => $form->createView()]);
   }
 }
