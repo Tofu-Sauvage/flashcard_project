ba8ea61a662a0674eda942f33c5abad2d3ccfab7
QlNaga <pupier.quentin@gmail.com>
2021-01-04 16:27:10 +0100

    Section "cartes" et "decks" en cours de création

--- src/Controller/DeckboardController.php
+++ src/Controller/DeckboardController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\CardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DeckboardController extends AbstractController {
 
-  public function indexAction() {
-
-    return $this->render("base.html.twig");
+  public function indexAction(CardRepository $cardRepository) {
+    $lastCards = $cardRepository->findOneBy([], ['id' => 'desc']);
+    // dd($lastCards);
+    $lastCards = null;
+    return $this->render("pages/user/deckboard.html.twig", ["cards" => $lastCards]);
   }
 }
