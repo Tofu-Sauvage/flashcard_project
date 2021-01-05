b6e43b8f8378fc79638cbbb0001021c8c5093d16
Frelon-K <frelonk.games@gmail.com>
2021-01-05 16:13:42 +0100

    back end formulaire de deck

---
+++ src/Form/DeckType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Deck;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class DeckType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('created_at', DateType::class)
+            ->add('public', CheckboxType::class)
+            ->add('tags', TextType::class)
+            ->add('author', TextType::class)
+            ->add('fans', TextType::class)
+            ->add('cards', CardType::class)
+            ->add('langague_learn', LanguageType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Deck::class,
+        ]);
+    }
+}
