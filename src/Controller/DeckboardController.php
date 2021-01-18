5e1dfd2ed00f5c713611a4258ef2d98d887ef823
QlNaga <pupier.quentin@gmail.com>
2021-01-18 11:58:51 +0100

    commit avant merge

--- src/Controller/DeckboardController.php
+++ src/Controller/DeckboardController.php
@@ -4,24 +4,32 @@ namespace App\Controller;
 
 use App\Repository\CardRepository;
 use App\Repository\DeckRepository;
+use App\Repository\LanguageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use DateTime;
 
 class DeckboardController extends AbstractController {
 
-  public function indexAction(CardRepository $cardRepository, DeckRepository $deckRepository) {
+  public function indexAction(CardRepository $cardRepository, DeckRepository $deckRepository, LanguageRepository $languageRepository) {
     $idActiveUser = $this->getUser()->getId();
     $lastCard = $cardRepository->findOneBy(['author' => $idActiveUser], ['id' => 'desc']);
-    //  if ($lastCard->getCreatedAt() == null) {
-    //    $lastCard->setCreatedAt(new DateTime("now"));
-    //  }
+    
     $lastDeck = $deckRepository->findOneBy(['author' => $idActiveUser], ['id' => 'desc']);
     $listDeck = $deckRepository->findBy(['author' => $idActiveUser]);
 
+    $languageLearn = $languageRepository->findOneBy(['id' => $this->getUser()->getLanguageLearn()]);
+
     $listeFavouritedDecks = $this->getUser()->getFavorites();
     
-    $nombreDeDecksFavoris = Count($listeFavouritedDecks);
+    $nombreDeDecksFavoris = count($listeFavouritedDecks);
 
-    return $this->render("pages/user/deckboard.html.twig", ["last_card" => $lastCard, "last_deck" => $lastDeck, "list_deck" => $listDeck, "has_favs" => $nombreDeDecksFavoris, "list_deck_favoris" => $listeFavouritedDecks]);
+    return $this->render("pages/user/deckboard.html.twig", [
+      "last_card" => $lastCard, 
+      "last_deck" => $lastDeck, 
+      "list_deck" => $listDeck, 
+      "has_favs" => $nombreDeDecksFavoris, 
+      "list_deck_favoris" => $listeFavouritedDecks, 
+      "language_learn" => $languageLearn
+      ]);
   }
 }
