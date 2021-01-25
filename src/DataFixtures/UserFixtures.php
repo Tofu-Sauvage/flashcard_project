b54ec257382dfcb77d335d40fceac8584ba91438
Frelon-K <frelonk.games@gmail.com>
2021-01-25 14:49:53 +0100

    j'ai pas envie de commit alors merde

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -26,38 +26,19 @@ class UserFixtures extends Fixture
     {
         // LANGUAGES
         $french = new Language();
-        $french->setName("Français")->setFlag("flags/france.png
-        ");
+        $french->setName("Français")->setFlag(null);
         $manager->persist($french);
         $manager->flush();
 
         $english = new Language();
-        $english->setName("Anglais")->setFlag("flags/uk.png
-        ");
+        $english->setName("Anglais")->setFlag(null);
         $manager->persist($english);
         $manager->flush();
 
-        $finnois = new Language();
-        $finnois->setName("Finnois")->setFlag("flags/finland.png
-        ");
-        $manager->persist($finnois);
-        $manager->flush();
-
-        //CATEGORIES
-        $vocabulaire = new Category();
-        $vocabulaire->setName("Vocabulaire");
-        $manager->persist($vocabulaire);
-        $manager->flush();
-
-        $dicton = new Category();
-        $dicton->setName("Dicton");
-        $manager->persist($dicton);
-        $manager->flush();
-        
         // USERS
         $admin = new User();
-        $admin->setEmail("charlotte_sch@icloud.com")
-        ->setPassword($this->encoder->encodePassword($admin, 'flashcards'))
+        $admin->setEmail("admin@flash.com")
+        ->setPassword($this->encoder->encodePassword($admin, 'admin'))
         ->setRoles(['ROLE_ADMIN'])
         ->setName("Admin")
         ->setCreatedAt(new DateTime("now"))
@@ -66,96 +47,5 @@ class UserFixtures extends Fixture
         ->setImage("");
         $manager->persist($admin);
         $manager->flush();
-        
-        $jk = new User();
-        $jk->setEmail("toto@gmail.com")
-        ->setPassword($this->encoder->encodePassword($jk, 'toto'))
-        ->setRoles(['ROLE_USER'])
-        ->setName("Jean-Kevin")
-        ->setCreatedAt(new DateTime("now"))
-        ->setLanguageNative($french)
-        ->setLanguageLearn($english)
-        ->setImage("https://www.tezenis.com/on/demandware.static/-/Library-Sites-TezenisContentLibrary/default/dw1947c472/images/Landing/TZN_20191009_LP_Snoopy/all_LP_Area07_Character01_CW4019_Snoopy_DiscoverCollection_tzn.png
-        ");
-        $manager->persist($jk);
-        $manager->flush();
-
-        $Pepita = new User();
-        $Pepita->setEmail("titi@gmail.com")
-        ->setPassword($this->encoder->encodePassword($Pepita, 'toto'))
-        ->setRoles(['ROLE_USER'])
-        ->setName("Pepita")
-        ->setCreatedAt(new DateTime("now"))
-        ->setLanguageNative($french)
-        ->setLanguageLearn($english)
-        ->setImage("https://www.tezenis.com/on/demandware.static/-/Library-Sites-TezenisContentLibrary/default/dw1947c472/images/Landing/TZN_20191009_LP_Snoopy/all_LP_Area07_Character01_CW4019_Snoopy_DiscoverCollection_tzn.png
-        ");
-        $manager->persist($Pepita);
-        $manager->flush();
-
-        $Lulu91 = new User();
-        $Lulu91->setEmail("lulu@gmail.com")
-        ->setPassword($this->encoder->encodePassword($Lulu91, 'toto'))
-        ->setRoles(['ROLE_USER'])
-        ->setName("Lulu91")
-        ->setCreatedAt(new DateTime("now"))
-        ->setLanguageNative($french)
-        ->setLanguageLearn($finnois)
-        ->setImage("https://www.tezenis.com/on/demandware.static/-/Library-Sites-TezenisContentLibrary/default/dw1947c472/images/Landing/TZN_20191009_LP_Snoopy/all_LP_Area07_Character01_CW4019_Snoopy_DiscoverCollection_tzn.png
-        ");
-        $manager->persist($Lulu91);
-        $manager->flush();
-
-        $Kiki = new User();
-        $Kiki->setEmail("lala@gmail.com")
-        ->setPassword($this->encoder->encodePassword($Kiki, 'toto'))
-        ->setRoles(['ROLE_USER'])
-        ->setName("Kiki")
-        ->setCreatedAt(new DateTime("now"))
-        ->setLanguageNative($french)
-        ->setLanguageLearn($finnois)
-        ->setImage("https://www.tezenis.com/on/demandware.static/-/Library-Sites-TezenisContentLibrary/default/dw1947c472/images/Landing/TZN_20191009_LP_Snoopy/all_LP_Area07_Character01_CW4019_Snoopy_DiscoverCollection_tzn.png
-        ");
-        $manager->persist($Kiki);
-        $manager->flush();
-
-        //CARDS
-        for($i=0; $i<=5; $i++){
-            $card1 = new Card();
-            $card1->setQuestion("rabbit")->setAnswer("lapin")->setAuthor($Kiki)->setCategory($vocabulaire);
-            $manager->persist($card1);
-            $manager->flush();
-            }
-
-        for($i=0; $i<=5; $i++){
-            $card2 = new Card();
-            $card2->setQuestion("fox")->setAnswer("renard")->setAuthor($Lulu91)->setCategory($vocabulaire);
-            $manager->persist($card2);
-            $manager->flush();
-        }
-
-        for($i=0; $i<=3; $i++){
-            $card3 = new Card();
-            $card3->setQuestion("What the fuck !")->setAnswer("Ouate de phoque !")->setAuthor($Lulu91)->setCategory($dicton);
-            $manager->persist($card3);
-            $manager->flush();
-        }
-        
-        //DECKS
-        $deck1 = new Deck();
-        $deck1->setName("Deck1")->setDescription("Une description du deck 1")->setPublic("true")->setAuthor($Lulu91)->setLangagueLearn($english);
-        $deck1->addCard($card2);
-        $deck1->addCard($card3);
-        $deck1->addCard($card1);
-        $manager->persist($deck1);
-        $manager->flush();
-
-        $deck2 = new Deck();
-        $deck2->setName("Deck2")->setDescription("Une description du deck 2")->setPublic("false")->setAuthor($Lulu91)->setLangagueLearn($english);
-        $deck2->addCard($card1);
-        $deck1->addCard($card2);
-        $manager->persist($deck2);
-        $manager->flush();
-
     }
 }
